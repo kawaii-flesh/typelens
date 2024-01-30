7702710492b9a5171b388324a3d3d2f5954ed27f
kawaii-flesh <kawaiiflesh@yandex.ru>
2024-01-30 20:23:00 +0000

    ESlint; Prettier; Refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,48 @@
-"use strict";
-import * as vscode from "vscode";
-import { commands } from "vscode";
-import { TSCodeLensProvider } from "./tSCodeLensProvider";
+'use strict'
+import * as vscode from 'vscode'
+import { commands } from 'vscode'
+import { TSCodeLensProvider } from './tSCodeLensProvider'
 
 async function triggerCodeLensComputation() {
-  if (!vscode.window.activeTextEditor) return;
-  var end = vscode.window.activeTextEditor.selection.end;
-  await vscode.window.activeTextEditor.edit((editbuilder) => {
-    editbuilder.insert(end, " ");
-  });
-  await commands.executeCommand("undo");
+    if (!vscode.window.activeTextEditor) return
+    const end = vscode.window.activeTextEditor.selection.end
+    await vscode.window.activeTextEditor.edit(editbuilder => {
+        editbuilder.insert(end, ' ')
+    })
+    await commands.executeCommand('undo')
 }
 
 function log() {
-  if (!vscode.window.activeTextEditor) return;
-  const output = vscode.window.createOutputChannel("typelens");
-  output.appendLine("Hello");
-  output.show();
+    if (!vscode.window.activeTextEditor) return
+    const output = vscode.window.createOutputChannel('typelens')
+    output.appendLine('Hello')
+    output.show()
 }
 
-function setUpCommands(
-  disposables: vscode.Disposable[],
-  provider: TSCodeLensProvider
-) {
-  disposables.push(
-    commands.registerCommand("typelens.toggle", async () => {
-      provider.config.typeLensEnabled = !provider.config.typeLensEnabled;
-      await triggerCodeLensComputation();
-    })
-  );
-  disposables.push(
-    commands.registerCommand("typelens.log", () => {
-      log();
-    })
-  );
+function setUpCommands(disposables: vscode.Disposable[], provider: TSCodeLensProvider) {
+    disposables.push(
+        commands.registerCommand('typelens.toggle', async () => {
+            provider.config.typeLensEnabled = !provider.config.typeLensEnabled
+            await triggerCodeLensComputation()
+        }),
+    )
+    disposables.push(
+        commands.registerCommand('typelens.log', () => {
+            log()
+        }),
+    )
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  const provider = new TSCodeLensProvider();
-  const disposables: vscode.Disposable[] = context.subscriptions;
-  setUpCommands(disposables, provider);
-  disposables.push(vscode.languages.registerCodeLensProvider(["*"], provider));
-  disposables.push(
-    vscode.window.onDidChangeActiveTextEditor((editor) => {
-      if (editor) {
-        provider.updateDecorations(editor.document.uri);
-      }
-    })
-  );
+    const provider = new TSCodeLensProvider()
+    const disposables: vscode.Disposable[] = context.subscriptions
+    setUpCommands(disposables, provider)
+    disposables.push(vscode.languages.registerCodeLensProvider(['*'], provider))
+    disposables.push(
+        vscode.window.onDidChangeActiveTextEditor(editor => {
+            if (editor) {
+                provider.updateDecorations(editor.document.uri)
+            }
+        }),
+    )
 }
--- src/symbolKindSet.ts
+++ src/symbolKindSet.ts
@@ -1,24 +1,19 @@
-import { SymbolKind } from "vscode";
+import { SymbolKind } from 'vscode'
 
 export const standardSymbolKindSet = [
-  SymbolKind.Method,
-  SymbolKind.Function,
-  SymbolKind.Property,
-  SymbolKind.Class,
-  SymbolKind.Interface,
-  SymbolKind.Enum,
-  SymbolKind.Constant,
-  SymbolKind.Variable,
-];
-export const cssSymbolKindSet = [
-  SymbolKind.Method,
-  SymbolKind.Function,
-  SymbolKind.Property,
-  SymbolKind.Variable,
-];
+    SymbolKind.Method,
+    SymbolKind.Function,
+    SymbolKind.Property,
+    SymbolKind.Class,
+    SymbolKind.Interface,
+    SymbolKind.Enum,
+    SymbolKind.Constant,
+    SymbolKind.Variable,
+]
+export const cssSymbolKindSet = [SymbolKind.Method, SymbolKind.Function, SymbolKind.Property, SymbolKind.Variable]
 export const SymbolKindInterst = {
-  scss: cssSymbolKindSet,
-  less: cssSymbolKindSet,
-  typescript: standardSymbolKindSet,
-  javascript: standardSymbolKindSet,
-};
+    scss: cssSymbolKindSet,
+    less: cssSymbolKindSet,
+    typescript: standardSymbolKindSet,
+    javascript: standardSymbolKindSet,
+}
